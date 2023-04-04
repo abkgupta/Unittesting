9d5272e79ead0f4a7dc79500b5e43940f778c5fd
abkgupta <abk1379gupta@gmail.com>
2023-04-04 18:50:19 +0530

    Unit testing in Angular

---
+++ src/app/user/user.component.spec.ts
@@ -0,0 +1,29 @@
+import { async,ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UserComponent } from './user.component';
+
+describe('UserComponent', () => {
+  let component: UserComponent;
+  let fixture: ComponentFixture<UserComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ UserComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(UserComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+  it("testing title",()=>{
+    expect(component.componentName).toBe("user")
+  })
+  it("testing function",()=>{
+    expect(component.sum(40,60)).toBe(100)
+  })
+});
